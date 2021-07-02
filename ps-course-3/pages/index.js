ecfd46cc1b98dd6ed0c118db81e96a4483d5fd8a
Rhian <rhian.luis.cs+github@gmail.com>
2021-07-02 18:35:21 -0300

    Module 3 - Lesson 6

--- ps-course-3/pages/index.js
+++ ps-course-3/pages/index.js
@@ -1,5 +1,60 @@
+import { data } from "../SpeakerData";
+
 const IndexPage = () => {
-  return <div>Hello From Pluralsight!</div>;
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map((speaker) => {
+          const {
+            id,
+            bio,
+            first,
+            last,
+            favorite,
+            twitterHandle,
+            company,
+            sessions,
+          } = speaker;
+
+          return (
+            <div
+              key={id}
+              className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12"
+            >
+              <div className="card card-height p-4 mt-4">
+                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                  <img
+                    src={`/images/speaker-${id}.jpg`}
+                    width="300"
+                    alt={`${first} ${last}`}
+                    className="contain-fit"
+                  />
+                </div>
+                <div className="speaker-info">
+                  <div className="d-flex justify-content-between mb-3">
+                    <h3 className="text-truncate w-200">
+                      {first} {last}
+                    </h3>
+                  </div>
+                  <div>
+                    <p className="pb-4">
+                      {bio} {company} {twitterHandle} {favorite}
+                    </p>
+                  </div>
+                  <div className="sessionBox card h-250">
+                    <div className="session w-100">
+                      {sessions[0].title}{" "}
+                      <strong>Room: {sessions[0].room.name}</strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
