6266e5cb6f31243d4f1a3b49d6e7c5a8baaadb10
Rhian <rhian.luis.cs+github@gmail.com>
2021-07-09 16:29:53 -0300

    Add basic item form

--- workout-log/src/components/App.js
+++ workout-log/src/components/App.js
@@ -1,13 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
 import ItemsTable from './ItemsTable';
 import InsertItemForm from './InsertItemForm';
 
 export function App({ initialData }) {
+  const [workouts, setWorkouts] = useState([]);
+
+  const addWorkout = (newWorkout) => {
+    setWorkouts((previousWorkouts) => [...previousWorkouts, newWorkout]);
+    console.log(workouts);
+  };
+
   return (
     <div className="container">
       <h1 className="centered-heading">{initialData.appName}</h1>
-      <InsertItemForm />
+      <InsertItemForm onSubmit={addWorkout} />
       <hr />
       <ItemsTable />
       <h1 className="centered-heading">10 hours of exercises</h1>
--- workout-log/src/components/InsertItemForm.js
+++ workout-log/src/components/InsertItemForm.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-const InsertItemForm = () => (
-  <div className="insert-item-form">
-    <input type="number" />
-    <select name="type">
-      <option value="running">Running</option>
-      <option value="swimming">Swimming</option>
-      <option value="bicycle">Bicycle</option>
-    </select>
-    <input type="date" name="date" />
-    <input type="submit" value="Add" />
-  </div>
-);
+const itemReducer = (itemState, event) => {
+  return {
+    ...itemState,
+    [event.name]: event.value,
+  };
+};
+
+const InsertItemForm = ({ onSubmit }) => {
+  const [item, setItem] = useReducer(itemReducer, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(item);
+  };
+
+  const handleChange = (event) => {
+    setItem({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} target="#" className="insert-item-form">
+      <input type="number" name="hours" onChange={handleChange} />
+      <select name="type" onChange={handleChange}>
+        <option value="running">Running</option>
+        <option value="swimming">Swimming</option>
+        <option value="bicycle">Bicycle</option>
+      </select>
+      <input type="date" name="date" onChange={handleChange} />
+      <input type="submit" value="Add" />
+    </form>
+  );
+};
 
 export default InsertItemForm;
