59420ba80bf8d8b3d88fb2c7813fa1af5b19ef04
Rhian <rhian.luis.cs+github@gmail.com>
2021-07-12 23:14:01 -0300

    Improve responsive design

--- workout-log/src/components/WorkoutsTable.js
+++ workout-log/src/components/WorkoutsTable.js
@@ -2,27 +2,29 @@ import React from 'react';
 import WorkoutTableItem from './WorkoutTableItem';
 
 const WorkoutsTable = ({ workouts, onDestroy }) => (
-  <table className="items-table">
-    <thead>
-      <tr>
-        <th>Tempo</th>
-        <th>Tipo</th>
-        <th>Data</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {workouts.map((workout) => {
-        return (
-          <WorkoutTableItem
-            key={workout.uuid}
-            {...workout}
-            onDestroy={onDestroy}
-          />
-        );
-      })}
-    </tbody>
-  </table>
+  <div style={{ 'overflow-x': 'auto' }}>
+    <table className="items-table">
+      <thead>
+        <tr>
+          <th>Tempo</th>
+          <th>Tipo</th>
+          <th>Data</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {workouts.map((workout) => {
+          return (
+            <WorkoutTableItem
+              key={workout.uuid}
+              {...workout}
+              onDestroy={onDestroy}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default WorkoutsTable;
